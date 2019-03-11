1762dcb589188927bffbdb7bda8ff351e2bb9171
Hitesh Batra <32892304+hiteshbatra1998@users.noreply.github.com>
2019-03-11 14:58:21 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SeacrBar from './SearchBar.js';
+const App =() =>{
+    return (
+        <div className="ui container" style={{marginTop:'10px'}}>
+            <SeacrBar />
+        </div>
+        );
+}
+export default App;
