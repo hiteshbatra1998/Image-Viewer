0af7c7bbf918e56c7e891f9fe9d0a1748613392c
Hitesh Batra <32892304+hiteshbatra1998@users.noreply.github.com>
2019-03-12 23:23:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import SeacrBar from './SearchBar.js';
-const App =() =>{
-    return (
-        <div className="ui container" style={{marginTop:'10px'}}>
-            <SeacrBar />
-        </div>
-        );
-}
+import unsplash from '../api/unsplash'
+import SearchBar from './SearchBar.js';
+class App extends React.Component{
+    state ={ images :[] };
+    async onSearchSubmit(term) {
+        const response = await unsplash.get('/search/photos' , {
+            params: { query : term}
+        });
+        this.setState({ images: response.data.results })
+    }
+    render(){
+        return (
+            <div className="ui container" style={{marginTop:'10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit.bind(this)} />
+                Found {this.state.images.length} images
+            </div>
+            );
+        }
+};
 export default App;
