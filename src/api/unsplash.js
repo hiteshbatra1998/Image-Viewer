ce2bde63f24994c22a0ee9b312689fb68f5a4068
Hitesh Batra <32892304+hiteshbatra1998@users.noreply.github.com>
2019-03-12 23:22:30 +0530

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL:'https://api.unsplash.com',
     headers:{
-        Authorization : 'Client-ID b2af035ab9a9cd4422fb6f612a85ddff1eb0895d3ebbfcdd98d2d113c4fe6360'
+        Authorization : 'Client-ID Your Id'  // put your ap key to run the project
     }
 });
